0a2c0cc05cea8c23e377d8790e5846502c5a064a
YaelDuarte <yaelduarte48@aragon.unam.mx>
2024-11-28 19:25:08 -0600

    #Cambios Boton

--- Amazon2/pag-comp-vende/comp-vende/ScriptCom.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btn-iniciar-sesion').addEventListener('click', () => {
-    window.location.href = 'IndexLog.html'; //Ruta de acceso del Archivo
-});
-document.getElementById('btn-vender').addEventListener('click', () =>{
-    window.location.href = 'IndexVen.html';
-})
