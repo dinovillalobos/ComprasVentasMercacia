64f715927ac16fa676e0a81ff1d848f39dda221f
dinovillalobos <villalobosdoricardo@gmail.com>
2024-11-30 14:15:12 -0600

    Pruaba BD

---
+++ Amazon2/Registro.php
@@ -0,0 +1,33 @@
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "usuarios";
+
+        // Crear conexión
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Verificar conexión
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
+        }
+
+        $nombre = $_POST['nombre'];
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO usuarios (nombre, email, password) VALUES ('$nombre', '$email', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Registro exitoso";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
+    ?>
+</body>
+</html>
